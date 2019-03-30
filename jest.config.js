650f26102cebae76cb5c772fc97e22cb7a6811e7
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-03-30 18:02:04 +0100

    chore: support full-source coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     unmockedModulePathPatterns: ['<rootDir>/src/__tests__'],
 
+    collectCoverageFrom: ['src/**/*.(js|jsx|ts|tsx)'],
+
     // Setup react-testing-library
     setupFilesAfterEnv: ['<rootDir>/src/setupRTL.ts'],
 }
