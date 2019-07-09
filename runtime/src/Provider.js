99d0fc182bd4bfa382821f7a2580013868124a17
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-07-09 17:46:56 +0200

    chore: explicitly nest providers

--- runtime/src/Provider.js
+++ runtime/src/Provider.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import { Provider as ConfigProvider } from '@dhis2/app-service-config'
 import { Provider as DataProvider } from '@dhis2/app-service-data'
-import { CurriedProviders } from './utils/CurriedProviders'
-
-const serviceProviders = [DataProvider]
 
 export const Provider = ({ config, children }) => (
     <ConfigProvider config={config}>
-        <CurriedProviders providers={serviceProviders}>
-            {children}
-        </CurriedProviders>
+        <DataProvider>{children}</DataProvider>
     </ConfigProvider>
 )
