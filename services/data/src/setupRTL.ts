becae1a3bd5c4ac8e0f3677fae240593f39f76e5
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-08-13 15:02:22 +0200

    chore: consolidate dev dependencies to workspace root (#65)
    * chore: consolidate dev dependencies to workspace root
    
    * chore: upgrade deps
    
    * chore: update example deps
    
    * chore: update deps, move react to top-level, add bundle analyzer

--- services/data/src/setupRTL.ts
+++ services/data/src/setupRTL.ts
@@ -1,5 +1,4 @@
-import 'jest-dom/extend-expect'
-import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
 
 process.on('unhandledRejection', err => {
     throw err
