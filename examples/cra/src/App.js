28ae153ae0658641c466c3c2e4f9adaba355aa20
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-08-14 19:08:55 +0200

    chore: move config parsing outside of App component

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -4,15 +4,16 @@ import { SwitchableProvider } from './components/SwitchableProvider'
 import { ConfigConsumer } from './components/ConfigConsumer'
 import { IndicatorList } from './components/IndicatorList'
 
+const config = {
+    baseUrl: process.env.REACT_APP_D2_BASE_URL || 'https://play.dhis2.org/dev',
+    apiVersion: process.env.REACT_APP_D2_API_VERSION || 33,
+}
+
+const providerType = (
+    process.env.REACT_APP_D2_PROVIDER_TYPE || 'runtime'
+).toLowerCase()
+
 const App = () => {
-    const config = {
-        baseUrl:
-            process.env.REACT_APP_D2_BASE_URL || 'https://play.dhis2.org/dev',
-        apiVersion: process.env.REACT_APP_D2_API_VERSION || 33,
-    }
-    const providerType = (
-        process.env.REACT_APP_D2_PROVIDER_TYPE || 'runtime'
-    ).toLowerCase()
     return (
         <SwitchableProvider type={providerType} config={config}>
             <div className="App">
