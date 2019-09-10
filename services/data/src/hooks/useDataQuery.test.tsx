3d3f12608cb6d206db58cf76e57ed2dc3ecc64c8
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-09-10 19:11:30 +0200

    chore: fix capitalization typo

--- services/data/src/hooks/useDataQuery.test.tsx
+++ services/data/src/hooks/useDataQuery.test.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-import { useDataQuery } from './usedataquery'
+import { useDataQuery } from './useDataQuery'
 import { renderHook, act } from '@testing-library/react-hooks'
 import { CustomDataProvider } from '../components/CustomDataProvider'
 
