24633b6f0671d7d4155975b55008cbfa5d90c161
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-09-09 14:58:40 +0200

    feat(persist queries): use versioning

--- examples/query-playground/src/hooks/useTabs.js
+++ examples/query-playground/src/hooks/useTabs.js
@@ -1,5 +1,13 @@
 import { useConfig } from '@dhis2/app-runtime'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+/*
+ * If the shape of "tabTemplate" changes, by increasing the version number,
+ * it'll prevent an error when parsing the existing cached data
+ *
+ * Is a string as localStorage only stores strings
+ */
+const VERSION = '1'
 
 const tabTemplate = {
     active: false,
@@ -29,22 +37,41 @@ const setValue = ({ key, tabs, setTabs }) => index => value => {
     return setTabs([...tabs.slice(0, index), update, ...tabs.slice(index + 2)])
 }
 
+const useShouldReset = storageNameSpace => {
+    const storageVersionName = `${storageNameSpace}.version`
+    const lastVersion = localStorage.getItem(storageVersionName)
+
+    useEffect(() => {
+        localStorage.setItem(storageVersionName, VERSION)
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+    return !lastVersion || lastVersion !== VERSION
+}
+
+const storeTabs = (storageNameSpace, tabs) => {
+    // Don't store results
+    const storageTabs = tabs.map(tab => ({ ...tab, result: '' }))
+    localStorage.setItem(storageNameSpace, JSON.stringify(storageTabs))
+}
+
 export const useTabs = () => {
     const { baseUrl } = useConfig()
     const storageNameSpace = `${baseUrl}-playground`
+    const shouldReset = useShouldReset(storageNameSpace)
     const [tabs, _setTabs] = useState(
-        localStorage.getItem(storageNameSpace)
+        !shouldReset && localStorage.getItem(storageNameSpace)
             ? JSON.parse(localStorage.getItem(storageNameSpace))
             : addTab([])
     )
 
     const setTabs = tabs => {
-        // Don't store results
-        const storageTabs = tabs.map(tab => ({ ...tab, result: '' }))
-        localStorage.setItem(storageNameSpace, JSON.stringify(storageTabs))
-
+        storeTabs(storageNameSpace, tabs)
         _setTabs(tabs)
     }
+
+    useEffect(() => {
+        storeTabs(storageNameSpace, tabs)
+    }, [shouldReset]) // eslint-disable-line react-hooks/exhaustive-deps
 
     const setQuery = setValue({ key: 'query', tabs, setTabs })
     const setResult = setValue({ key: 'result', tabs, setTabs })
