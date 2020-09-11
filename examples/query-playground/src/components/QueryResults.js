a85d033744249e06466365b941cb2e278425f421
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-09-11 16:03:03 +0200

    refactor(styles jsx): change remaining styles jsx files to css modules

--- examples/query-playground/src/components/QueryResults.js
+++ examples/query-playground/src/components/QueryResults.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styles from './QueryResults.styles'
+import styles from './QueryResults.module.css'
 import { Editor } from './Editor'
 import i18n from '@dhis2/d2-i18n'
 
 export const QueryResults = ({ result }) => {
     return (
-        <div className="results">
+        <div className={styles.results}>
             <style jsx>{styles}</style>
             <Editor
                 value={result}
