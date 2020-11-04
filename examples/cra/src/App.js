0d1029baadda0d20e1266be977b06dd8f8a38000
Hendrik de Graaf <hendrik@dhis2.org>
2020-11-04 09:08:42 +0100

    chore(alerts-service): cleanup demo components

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -4,7 +4,6 @@ import { SwitchableProvider } from './components/SwitchableProvider'
 import { ConfigConsumer } from './components/ConfigConsumer'
 import { IndicatorList } from './components/IndicatorList'
 import { Alerts } from './components/Alerts'
-import { Alerter } from './components/Alerter'
 
 const config = {
     baseUrl: process.env.REACT_APP_D2_BASE_URL || 'http://localhost:8080',
@@ -22,7 +21,6 @@ const App = () => {
                 <header className="App-header">
                     <ConfigConsumer />
                     <IndicatorList />
-                    <Alerter />
                     <Alerts />
                 </header>
             </div>
