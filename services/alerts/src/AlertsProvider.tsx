f5239dabc494e04544cb4c85939ebfeac4bde48b
Hendrik de Graaf <hendrik@dhis2.org>
2020-11-10 15:14:50 +0100

    refactor(alerts-service): lazy initialise the alertsManager on the state

--- services/alerts/src/AlertsProvider.tsx
+++ services/alerts/src/AlertsProvider.tsx
@@ -1,13 +1,12 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState } from 'react'
 import { AlertsContext } from './AlertsContext'
 import { AlertsManagerContext, makeAlertsManager } from './AlertsManagerContext'
 import { Alert, AlertsManager } from './types'
 
 export const AlertsProvider = ({ children }: { children: React.ReactNode }) => {
     const [alerts, setAlerts] = useState<Alert[]>([])
-    const alertsManager: AlertsManager = useMemo(
-        () => makeAlertsManager(setAlerts),
-        []
+    const [alertsManager] = useState<AlertsManager>(
+        (): AlertsManager => makeAlertsManager(setAlerts)
     )
 
     return (
