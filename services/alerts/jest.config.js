b8f2fe9bc7635ded1faeadbf746055ebcc3db803
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-03-03 18:24:05 +0100

    chore: use default babel instead of ts-jest

--- services/alerts/jest.config.js
+++ services/alerts/jest.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    unmockedModulePathPatterns: ['<rootDir>/src/__tests__'],
-
-    collectCoverageFrom: ['src/**/*.(js|jsx|ts|tsx)'],
+    collectCoverageFrom: [
+        'src/**/*.(js|jsx|ts|tsx)',
+        '!src/index.ts',
+        '!src/types.ts',
+    ],
 
     // Setup react-testing-library
     setupFilesAfterEnv: ['<rootDir>/src/setupRTL.ts'],
