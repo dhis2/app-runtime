97d94976692688abcb4567eed7ca85f57d16f0a7
ismay <ismay@ismaywolff.nl>
2021-04-28 14:28:38 +0200

    test(dataquery): test callbacks

--- services/data/src/react/hooks/useDataQuery.test.tsx
+++ services/data/src/react/hooks/useDataQuery.test.tsx
@@ -5,11 +5,28 @@ import { cache, SWRConfig } from 'swr'
 import { CustomDataProvider } from '../components/CustomDataProvider'
 import { useDataQuery } from './useDataQuery'
 
+// eslint-disable-next-line react/display-name
+const createWrapper = mockData => ({ children }: { children?: ReactNode }) => {
+    return (
+        <SWRConfig value={{ dedupingInterval: 0 }}>
+            <CustomDataProvider data={mockData}>{children}</CustomDataProvider>
+        </SWRConfig>
+    )
+}
+
 describe('useDataQuery', () => {
     const originalError = console.error
 
+    beforeAll(() => {
+        // TODO: suppresses the act warning, we should address this
+        console.error = jest.fn()
+    })
+
     afterEach(async () => {
         await waitFor(() => cache.clear())
+    })
+
+    afterAll(() => {
         console.error = originalError
     })
 
@@ -17,18 +34,7 @@ describe('useDataQuery', () => {
         it('Should set initial loading state to true if not lazy', async () => {
             const query = { x: { resource: 'answer' } }
             const mockData = { answer: 42 }
-            const wrapper = ({ children }: { children?: ReactNode }) => {
-                return (
-                    <SWRConfig value={{ dedupingInterval: 0 }}>
-                        <CustomDataProvider data={mockData}>
-                            {children}
-                        </CustomDataProvider>
-                    </SWRConfig>
-                )
-            }
-
-            // TODO: suppresses the act warning, we should address this
-            console.error = jest.fn()
+            const wrapper = createWrapper(mockData)
 
             const { result } = renderHook(
                 () => useDataQuery(query, { lazy: false }),
@@ -42,22 +48,62 @@ describe('useDataQuery', () => {
         })
     })
 
+    describe('onComplete', () => {
+        it('Should call onComplete with the data after a successful fetch', async () => {
+            const query = { x: { resource: 'answer' } }
+            const mockData = { answer: 42 }
+            const wrapper = createWrapper(mockData)
+            const onComplete = jest.fn()
+            const onError = jest.fn()
+
+            const { result } = renderHook(
+                () => useDataQuery(query, { lazy: false, onComplete, onError }),
+                { wrapper }
+            )
+
+            await waitFor(() => {
+                expect(result.current).toMatchObject({
+                    data: { x: 42 },
+                })
+                expect(onComplete).toHaveBeenCalledWith({ x: 42 })
+                expect(onError).not.toHaveBeenCalled()
+            })
+        })
+    })
+
+    describe('onError', () => {
+        it('Should call onError with the error after an error', async () => {
+            const expectedError = new Error('Something went wrong')
+            const query = { x: { resource: 'answer' } }
+            const mockData = {
+                answer: () => {
+                    throw expectedError
+                },
+            }
+            const wrapper = createWrapper(mockData)
+            const onComplete = jest.fn()
+            const onError = jest.fn()
+
+            const { result, waitFor } = renderHook(
+                () => useDataQuery(query, { onError, onComplete }),
+                { wrapper }
+            )
+
+            await waitFor(() => {
+                expect(result.current).toMatchObject({
+                    error: expectedError,
+                })
+                expect(onError).toHaveBeenCalledWith(expectedError)
+                expect(onComplete).not.toHaveBeenCalled()
+            })
+        })
+    })
+
     describe('Data', () => {
         it('Should return the data on success', async () => {
             const query = { x: { resource: 'answer' } }
             const mockData = { answer: 42 }
-            const wrapper = ({ children }: { children?: ReactNode }) => {
-                return (
-                    <SWRConfig value={{ dedupingInterval: 0 }}>
-                        <CustomDataProvider data={mockData}>
-                            {children}
-                        </CustomDataProvider>
-                    </SWRConfig>
-                )
-            }
-
-            // TODO: suppresses the act warning, we should address this
-            console.error = jest.fn()
+            const wrapper = createWrapper(mockData)
 
             const { result, waitFor } = renderHook(() => useDataQuery(query), {
                 wrapper,
@@ -87,18 +133,7 @@ describe('useDataQuery', () => {
                     throw expectedError
                 },
             }
-            const wrapper = ({ children }: { children?: ReactNode }) => {
-                return (
-                    <SWRConfig value={{ dedupingInterval: 0 }}>
-                        <CustomDataProvider data={mockData}>
-                            {children}
-                        </CustomDataProvider>
-                    </SWRConfig>
-                )
-            }
-
-            // TODO: suppresses the act warning, we should address this
-            console.error = jest.fn()
+            const wrapper = createWrapper(mockData)
 
             const { result, waitFor } = renderHook(() => useDataQuery(query), {
                 wrapper,
@@ -123,18 +158,7 @@ describe('useDataQuery', () => {
         it('Should not fetch until refetch has been called if lazy', async () => {
             const query = { x: { resource: 'answer' } }
             const mockData = { answer: 42 }
-            const wrapper = ({ children }: { children?: ReactNode }) => {
-                return (
-                    <SWRConfig value={{ dedupingInterval: 0 }}>
-                        <CustomDataProvider data={mockData}>
-                            {children}
-                        </CustomDataProvider>
-                    </SWRConfig>
-                )
-            }
-
-            // TODO: suppresses the act warning, we should address this
-            console.error = jest.fn()
+            const wrapper = createWrapper(mockData)
 
             const { result, waitFor } = renderHook(
                 () => useDataQuery(query, { lazy: true }),
