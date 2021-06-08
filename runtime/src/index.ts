a512495da551c8c8c8170edf9f06f190bf60b5ca
Kai Vandivier <kpvandivier@gmail.com>
2021-06-08 16:23:14 +0200

    chore: remove offline service reexport until published

--- runtime/src/index.ts
+++ runtime/src/index.ts
@@ -12,10 +12,4 @@ export { useConfig } from '@dhis2/app-service-config'
 
 export { useAlerts, useAlert } from '@dhis2/app-service-alerts'
 
-export {
-    OfflineProvider,
-    CacheableSection,
-    useCacheableSection,
-} from '@dhis2/app-service-offline'
-
 export { Provider } from './Provider'
