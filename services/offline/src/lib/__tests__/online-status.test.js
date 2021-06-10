3a4d4c8229741fba82b66acbb05d37119b6e6654
Kai Vandivier <kpvandivier@gmail.com>
2021-06-11 00:12:23 +0200

    test: add browser event and throttling tests

--- services/offline/src/lib/__tests__/online-status.test.js
+++ services/offline/src/lib/__tests__/online-status.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react-hooks'
 import { useOnlineStatus } from '../online-status'
 
 beforeEach(() => {
@@ -23,6 +23,87 @@ describe('initalizes to navigator.onLine value', () => {
     })
 })
 
-test.todo('state changes in response to browser "online" and "offline" events')
+describe('state changes in response to browser "online" and "offline" events', () => {
+    it('switches from online to offline when the "offline" event triggers', () => {
+        jest.spyOn(navigator, 'onLine', 'get').mockReturnValueOnce(true)
+        // Capture callback to trigger later using addEventListener mock
+        const events = {}
+        window.addEventListener = jest.fn((event, cb) => (events[event] = cb))
+        const { result } = renderHook((...args) => useOnlineStatus(...args))
 
-test.todo('changes are throttled')
+        act(() => {
+            // Trigger callback captured by addEventListener mock
+            events.offline({ type: 'offline' })
+        })
+
+        expect(result.current.online).toBe(false)
+        expect(result.current.offline).toBe(true)
+    })
+
+    it('switches from offline to online when the "offline" event triggers', () => {
+        jest.spyOn(navigator, 'onLine', 'get').mockReturnValueOnce(false)
+        const events = {}
+        window.addEventListener = jest.fn((event, cb) => (events[event] = cb))
+        const { result } = renderHook((...args) => useOnlineStatus(...args))
+
+        act(() => {
+            events.online({ type: 'online' })
+        })
+
+        expect(result.current.online).toBe(true)
+        expect(result.current.offline).toBe(false)
+    })
+})
+
+describe('throttling state changes', () => {
+    it('throttles with a 1s delay by default', async () => {
+        // Start online
+        jest.spyOn(navigator, 'onLine', 'get').mockReturnValueOnce(true)
+        const events = {}
+        window.addEventListener = jest.fn((event, cb) => (events[event] = cb))
+        const { result, waitForNextUpdate } = renderHook((...args) =>
+            useOnlineStatus(...args)
+        )
+
+        await act(async () => {
+            // Multiple events in succession
+            events.offline({ type: 'offline' })
+            events.online({ type: 'online' })
+            events.offline({ type: 'offline' })
+            events.online({ type: 'online' })
+        })
+
+        // Immediately, only the offline should have triggered
+        expect(result.current.online).toBe(false)
+
+        // 1s later, final "online" event should finally resolve
+        await waitForNextUpdate({ timeout: 1000 })
+        expect(result.current.online).toBe(true)
+    })
+
+    it('can have throttle delay set to another number', async () => {
+        // Start online
+        jest.spyOn(navigator, 'onLine', 'get').mockReturnValueOnce(true)
+        const events = {}
+        window.addEventListener = jest.fn((event, cb) => (events[event] = cb))
+        const { result, waitForNextUpdate } = renderHook(
+            (...args) => useOnlineStatus(...args),
+            { initialProps: { throttleDelay: 50 } }
+        )
+
+        await act(async () => {
+            // Multiple events in succession
+            events.offline({ type: 'offline' })
+            events.online({ type: 'online' })
+            events.offline({ type: 'offline' })
+            events.online({ type: 'online' })
+        })
+
+        // Immediately, only the offline should have triggered
+        expect(result.current.online).toBe(false)
+
+        // 50ms later, final "online" event should finally resolve
+        await waitForNextUpdate({ timeout: 50 })
+        expect(result.current.online).toBe(true)
+    })
+})
