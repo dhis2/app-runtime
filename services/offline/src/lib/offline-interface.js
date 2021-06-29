d6c68ad0ad4c9ce0c8049ab03ea9956eacfafd06
Kai Vandivier <kpvandivier@gmail.com>
2021-06-29 16:52:30 +0200

    chore(offline-interface): comments

--- services/offline/src/lib/offline-interface.js
+++ services/offline/src/lib/offline-interface.js
@@ -4,11 +4,15 @@ import React, { createContext, useContext } from 'react'
 
 const OfflineContext = createContext()
 
-export function OfflineInterfaceProvider({
-    offlineInterface,
-    pwaEnabled,
-    children,
-}) {
+/**
+ * Receives an OfflineInterface instance as a prop (presumably from the app
+ * adapter) and provides it as context for other offline tools.
+ *
+ * On mount, it initializes the offline interface, which (among other things)
+ * checks for service worker updates and, if updates are ready, prompts the
+ * user with an alert to skip waiting and reload the page to use new content.
+ */
+export function OfflineInterfaceProvider({ offlineInterface, children }) {
     const { show } = useAlert(
         ({ message }) => message,
         ({ action, onConfirm }) => ({
@@ -18,7 +22,8 @@ export function OfflineInterfaceProvider({
     )
 
     React.useEffect(() => {
-        return offlineInterface.init({ promptUpdate: show, pwaEnabled })
+        // Init returns a tear-down function
+        return offlineInterface.init({ promptUpdate: show })
     }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
@@ -31,7 +36,6 @@ export function OfflineInterfaceProvider({
 OfflineInterfaceProvider.propTypes = {
     children: PropTypes.node,
     offlineInterface: PropTypes.shape({ init: PropTypes.func }),
-    pwaEnabled: PropTypes.bool,
 }
 
 export function useOfflineInterface() {
