c412d9f3889c6fdb8698a378e9a54eed65f67ffc
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-07-06 09:58:39 +0200

    test(hooks): make sure hooks work the way they should after update

--- services/data/src/react/hooks/useDataMutation.test.tsx
+++ services/data/src/react/hooks/useDataMutation.test.tsx
@@ -1,36 +1,53 @@
 import { renderHook, act } from '@testing-library/react-hooks'
-import React, { ReactNode } from 'react'
-import { CreateMutation } from '../../engine/types/Mutation'
+import * as React from 'react'
+import { ReactNode } from 'react'
+import { CreateMutation, UpdateMutation } from '../../engine/types/Mutation'
 import { CustomDataProvider } from '../components/CustomDataProvider'
 import { useDataMutation } from './useDataMutation'
 
+let value = null
 const customData = {
-    answer: 42,
+    answer: (_, query) => {
+        value = query.data.answer
+        return Promise.resolve({ value })
+    },
 }
 
 const wrapper = ({ children }: { children?: ReactNode }) => (
     <CustomDataProvider data={customData}>{children}</CustomDataProvider>
 )
 
-const mutation: CreateMutation = {
+const createMutation: CreateMutation = {
     type: 'create',
     resource: 'answer',
-    data: { answer: 42 },
+    data: jest.fn(() => ({ answer: 42 })),
 }
+
+const updateMutation: UpdateMutation = {
+    type: 'update',
+    id: 'id-0',
+    resource: 'answer',
+    data: jest.fn(() => ({ answer: 43 })),
+}
+
 describe('useDataMustation', () => {
     const originalError = console.error
 
+    beforeEach(() => {
+        console.error = jest.fn()
+    })
+
     afterEach(() => {
         console.error = originalError
+        createMutation.data.mockClear()
+        updateMutation.data.mockClear()
     })
 
     it('Should render without failing', async () => {
         let hookState: any
 
-        console.error = jest.fn()
-
         act(() => {
-            hookState = renderHook(() => useDataMutation(mutation), {
+            hookState = renderHook(() => useDataMutation(createMutation), {
                 wrapper,
             })
         })
@@ -50,11 +67,32 @@ describe('useDataMustation', () => {
     it('Should run immediately with lazy: false', async () => {
         let hookState: any
 
-        console.error = jest.fn()
+        act(() => {
+            hookState = renderHook(
+                () => useDataMutation(createMutation, { lazy: false }),
+                { wrapper }
+            )
+        })
+
+        let [, state] = hookState.result.current
+        expect(state).toMatchObject({ called: true, loading: true })
+
+        await hookState.waitForNextUpdate()
+
+        state = hookState.result.current[1]
+        expect(state).toMatchObject({
+            loading: false,
+            data: { value: 42 },
+        })
+    })
+
+    it('should', async () => {
+        let hookState: any
+        const mutations = [{ m1: createMutation }, { m2: updateMutation }]
 
         act(() => {
             hookState = renderHook(
-                () => useDataMutation(mutation, { lazy: false }),
+                () => useDataMutation(mutations, { lazy: false }),
                 {
                     wrapper,
                 }
@@ -67,6 +105,20 @@ describe('useDataMustation', () => {
         await hookState.waitForNextUpdate()
 
         state = hookState.result.current[1]
-        expect(state).toMatchObject({ loading: false, data: 42 })
+        expect(state).toMatchObject({
+            loading: false,
+            data: {
+                m1: { value: 42 },
+                m2: { value: 43 },
+            },
+        })
+
+        expect(createMutation.data).toHaveBeenCalledTimes(1)
+        expect(updateMutation.data).toHaveBeenCalledTimes(1)
+        expect(createMutation.data).toHaveBeenCalledWith({}, {})
+        expect(updateMutation.data).toHaveBeenCalledWith(
+            {},
+            { m1: { value: 42 } }
+        )
     })
 })
--- services/data/src/react/hooks/useDataQuery.test.tsx
+++ services/data/src/react/hooks/useDataQuery.test.tsx
@@ -1,32 +1,40 @@
 import { renderHook, act } from '@testing-library/react-hooks'
-import React, { ReactNode } from 'react'
+import * as React from 'react'
+import { ReactNode } from 'react'
 import { CustomDataProvider } from '../components/CustomDataProvider'
 import { useDataQuery } from './useDataQuery'
 
+let answer = 42
 const customData = {
-    answer: 42,
+    answer: () => Promise.resolve(answer++),
 }
 
 const wrapper = ({ children }: { children?: ReactNode }) => (
     <CustomDataProvider data={customData}>{children}</CustomDataProvider>
 )
 
-const query = {
-    x: {
-        resource: 'answer',
-    },
+const answerQuery = {
+    resource: 'answer',
+    params: jest.fn(() => ({})),
 }
+
+const query = { x: answerQuery }
+
 describe('useDataQuery', () => {
     const originalError = console.error
 
+    beforeEach(() => {
+        answer = 42
+        console.error = jest.fn()
+    })
+
     afterEach(() => {
         console.error = originalError
+        answerQuery.params.mockClear()
     })
 
     it('Should render without failing', async () => {
         let hookState: any
-
-        console.error = jest.fn()
 
         act(() => {
             hookState = renderHook(() => useDataQuery(query), {
@@ -39,8 +47,6 @@ describe('useDataQuery', () => {
 
     it('Should lazily await a refetch call', async () => {
         let hookState: any
-
-        console.error = jest.fn()
 
         act(() => {
             hookState = renderHook(() => useDataQuery(query, { lazy: true }), {
@@ -62,5 +68,31 @@ describe('useDataQuery', () => {
             loading: false,
             data: { x: 42 },
         })
+    })
+
+    it('should', async () => {
+        let hookState: any
+        const queries = [{ q1: answerQuery }, { q2: answerQuery }]
+
+        act(() => {
+            hookState = renderHook(() => useDataQuery(queries), {
+                wrapper,
+            })
+        })
+
+        let state = hookState.result.current
+        expect(state).toMatchObject({ loading: true })
+
+        await hookState.waitForNextUpdate()
+
+        state = hookState.result.current
+        expect(state).toMatchObject({
+            loading: false,
+            data: { q1: 42, q2: 43 },
+        })
+
+        expect(answerQuery.params).toHaveBeenCalledTimes(2)
+        expect(answerQuery.params).toHaveBeenNthCalledWith(1, {}, {})
+        expect(answerQuery.params).toHaveBeenNthCalledWith(2, {}, { q1: 42 })
     })
 })
