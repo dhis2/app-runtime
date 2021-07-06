a0ba3860e2c5a2f363c6f6b10ff07156246bf4d5
Kai Vandivier <kpvandivier@gmail.com>
2021-07-06 17:26:22 +0200

    test: expand offline provider tests

--- services/offline/src/__tests__/integration.test.js
+++ services/offline/src/__tests__/integration.test.js
@@ -4,13 +4,27 @@ import { OfflineProvider } from '../lib/offline-provider'
 
 const mockOfflineInterface = {
     init: jest.fn(),
-    startRecording: async () => null,
-    getCachedSections: async () => [],
-    removeSection: async () => null,
+    startRecording: jest.fn().mockResolvedValue(),
+    getCachedSections: jest.fn().mockResolvedValue(['TODO: Dummy sections']),
+    removeSection: jest.fn().mockResolvedValue(true),
 }
+
+afterEach(() => {
+    jest.clearAllMocks()
+})
 
 describe('Testing offline provider', () => {
     it('Should render without failing', async () => {
+        const wrapper = render(
+            <OfflineProvider offlineInterface={mockOfflineInterface}>
+                <div data-testid="test-div" />
+            </OfflineProvider>
+        )
+
+        expect(wrapper.findByTestId('test-div')).toBeDefined()
+    })
+
+    it('Should initialize the offline interface with an update prompt', () => {
         render(
             <OfflineProvider offlineInterface={mockOfflineInterface}>
                 {/* TODO */}
@@ -18,5 +32,10 @@ describe('Testing offline provider', () => {
         )
 
         expect(mockOfflineInterface.init).toHaveBeenCalledTimes(1)
+
+        // Expect to have been called with a 'promptUpdate' function
+        const arg = mockOfflineInterface.init.mock.calls[0][0]
+        expect(arg).toHaveProperty('promptUpdate')
+        expect(typeof arg['promptUpdate']).toBe('function')
     })
 })
