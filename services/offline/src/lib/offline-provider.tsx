ea7059805058a8ab052d491176c76c02991ee9c7
Kai Vandivier <kpvandivier@gmail.com>
2021-07-23 14:16:37 +0200

    refactor: add types to offline-provider

--- services/offline/src/lib/offline-provider.tsx
+++ services/offline/src/lib/offline-provider.tsx
@@ -3,8 +3,45 @@ import React from 'react'
 import { CacheableSectionProvider } from './cacheable-section-state'
 import { OfflineInterfaceProvider } from './offline-interface'
 
+interface PromptUpdate {
+    (params: { message: string; action: string; onConfirm: () => void }): void
+}
+
+interface StartRecording {
+    (params: {
+        sectionId: string
+        recordingTimeoutDelay: number
+        onStarted: () => void
+        onCompleted: () => void
+        onError: (err: Error) => void
+    }): Promise<undefined>
+}
+
+interface IndexedDBCachedSection {
+    sectionId: string
+    lastUpdated: Date
+    // this may change in the future:
+    requests: number
+}
+
+interface OfflineInterface {
+    readonly pwaEnabled: boolean
+    init: (params: { promptUpdate: PromptUpdate }) => () => void
+    startRecording: StartRecording
+    getCachedSections: () => Promise<IndexedDBCachedSection[]>
+    removeCachedSection: (id: string) => Promise<boolean>
+}
+
+interface OfflineProviderInput {
+    offlineInterface?: OfflineInterface
+    children: React.ReactNode
+}
+
 /** A context provider for all the relevant offline contexts */
-export function OfflineProvider({ offlineInterface, children }) {
+export function OfflineProvider({
+    offlineInterface,
+    children,
+}: OfflineProviderInput): React.ReactNode {
     // If an offline interface is not provided, or if one is provided and PWA
     // is not enabled, skip adding context providers
     if (!offlineInterface) {
