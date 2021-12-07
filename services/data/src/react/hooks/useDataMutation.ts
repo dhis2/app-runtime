5abdd59f1b3e1dc81c13ccd3d67724c5bce9fa73
ismay <7355199+ismay@users.noreply.github.com>
2021-12-07 16:39:06 +0100

    refactor(use-data-mutation): use react-query for mutations

--- services/data/src/react/hooks/useDataMutation.ts
+++ services/data/src/react/hooks/useDataMutation.ts
@@ -1,32 +1,38 @@
-import { useCallback } from 'react'
+import { useMutation } from 'react-query'
 import { QueryOptions, Mutation } from '../../engine'
 import { MutationRenderInput } from '../../types'
 import { useDataEngine } from './useDataEngine'
-import { useQueryExecutor } from './useQueryExecutor'
 import { useStaticInput } from './useStaticInput'
 
 const empty = {}
+
 export const useDataMutation = (
     mutation: Mutation,
-    { onComplete, onError, variables = empty, lazy = true }: QueryOptions = {}
+    {
+        onComplete,
+        onError,
+        variables: initialVariables = empty,
+        lazy = true,
+    }: QueryOptions = {}
 ): MutationRenderInput => {
     const engine = useDataEngine()
     const [theMutation] = useStaticInput<Mutation>(mutation, {
         warn: true,
         name: 'mutation',
     })
-    const execute = useCallback(
-        options => engine.mutate(theMutation, options),
-        [engine, theMutation]
-    )
-    const { refetch: mutate, called, loading, error, data } = useQueryExecutor({
-        execute,
-        variables,
-        singular: false,
-        immediate: !lazy,
-        onComplete,
-        onError,
-    })
 
-    return [mutate, { engine, called, loading, error, data }]
+    const mutationFn = variables =>
+        engine.mutate(theMutation, { ...initialVariables, ...variables })
+    const { data, mutateAsync, error, isLoading, isIdle } = useMutation(
+        mutationFn,
+        {
+            onSuccess: onComplete,
+            onError,
+        }
+    )
+
+    return [
+        mutateAsync,
+        { engine, called: !isIdle, loading: isLoading, error, data },
+    ]
 }
