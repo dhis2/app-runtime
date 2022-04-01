fee6fcecf6189a203fc94ed3ffca4c4f4fff0585
Austin McGee <947888+amcgee@users.noreply.github.com>
2022-04-01 13:43:48 +0200

    chore: move mergeAndCompareVariables to separate file, add tests, add react debug values

---
+++ services/data/src/react/hooks/mergeAndCompareVariables.ts
@@ -0,0 +1,32 @@
+import type { QueryVariables } from '../../engine'
+import { stableVariablesHash } from './stableVariablesHash'
+
+export const mergeAndCompareVariables = (
+    previousVariables?: QueryVariables,
+    newVariables?: QueryVariables,
+    previousHash?: string
+) => {
+    if (!newVariables) {
+        return {
+            identical: true,
+            mergedVariablesHash: previousHash,
+            mergedVariables: previousVariables,
+        }
+    }
+
+    // Use cached hash if it exists
+    const currentHash = previousHash || stableVariablesHash(previousVariables)
+
+    const mergedVariables = {
+        ...previousVariables,
+        ...newVariables,
+    }
+    const mergedVariablesHash = stableVariablesHash(mergedVariables)
+    const identical = currentHash === mergedVariablesHash
+
+    return {
+        identical,
+        mergedVariablesHash,
+        mergedVariables,
+    }
+}
