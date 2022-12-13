d2e5ffca2f2b483acc31267c9bf236070401ef71
Kai Vandivier <kpvandivier@gmail.com>
2022-12-13 16:17:19 +0100

    chore: other renaming clean-up

--- services/offline/src/index.ts
+++ services/offline/src/index.ts
@@ -1,6 +1,7 @@
 export { OfflineProvider } from './lib/offline-provider'
 export { CacheableSection, useCacheableSection } from './lib/cacheable-section'
 export { useCachedSections } from './lib/cacheable-section-state'
+// Use "useOnlineStatus" name for backwards compatibility
 export { useNetworkStatus as useOnlineStatus } from './lib/network-status'
 export { useOnlineStatusMessage } from './lib/online-status-message'
 export { clearSensitiveCaches } from './lib/clear-sensitive-caches'
