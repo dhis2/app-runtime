2a86c2d7036f695163cad2d72f213df2ed16e6db
Kai Vandivier <kpvandivier@gmail.com>
2022-12-13 13:28:24 +0100

    refactor: rename to NetworkStatus

--- services/offline/src/index.ts
+++ services/offline/src/index.ts
@@ -1,7 +1,7 @@
 export { OfflineProvider } from './lib/offline-provider'
 export { CacheableSection, useCacheableSection } from './lib/cacheable-section'
 export { useCachedSections } from './lib/cacheable-section-state'
-export { useOnlineStatus } from './lib/online-status'
+export { useNetworkStatus as useOnlineStatus } from './lib/network-status'
 export { useOnlineStatusMessage } from './lib/online-status-message'
 export { clearSensitiveCaches } from './lib/clear-sensitive-caches'
 export { useDhis2ConnectionStatus } from './lib/dhis2-connection-status'
