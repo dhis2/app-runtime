16431502b98d93dfc7b441fb172b6323bfdbdd87
Kai Vandivier <kpvandivier@gmail.com>
2023-05-05 08:21:38 +0200

    chore: fix comment

--- services/offline/src/lib/dhis2-connection-status/is-ping-available.ts
+++ services/offline/src/lib/dhis2-connection-status/is-ping-available.ts
@@ -4,7 +4,7 @@ import { Version } from '@dhis2/app-service-config'
  * Checks the server version to see if the /api/ping endpoint is available for
  * this version.
  *
- * The endpoint was released for versions 2.37.10, 2.38.3, 2.39.2, and 2.40.0
+ * The endpoint was released for versions 2.37.10, 2.38.4, 2.39.2, and 2.40.0
  * (see DHIS2-14531). All versions below that are considered unsupported
  *
  * If the patchVersion is undefined, it's assumed to be a dev server that's
