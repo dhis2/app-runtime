64fbe3e5811fb842fc22bcc0b4a351de6fcd40f4
Kai Vandivier <kpvandivier@gmail.com>
2024-02-28 22:23:47 +0100

    fix: ping endpoint for absolute paths

--- services/offline/src/lib/dhis2-connection-status/use-ping-query.ts
+++ services/offline/src/lib/dhis2-connection-status/use-ping-query.ts
@@ -8,7 +8,10 @@ export function usePingQuery(): () => Promise<any> {
 
     // This endpoint doesn't need any extra headers or handling since it's
     // public. It doesn't extend the user session. See DHIS2-14531
-    const ping = useCallback(() => fetch(baseUrl + '/api/ping'), [baseUrl])
+    const ping = useCallback(
+        () => fetch(new URL('./api/ping', baseUrl).href),
+        [baseUrl]
+    )
 
     return ping
 }
