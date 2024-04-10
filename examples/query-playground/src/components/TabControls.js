e1810a6d234ae8b0dbd071abca88c5dd7ac157df
Kai Vandivier <kpvandivier@gmail.com>
2024-04-10 12:05:48 +0200

    fix(query-playground): update dhis2 deps

--- examples/query-playground/src/components/TabControls.js
+++ examples/query-playground/src/components/TabControls.js
@@ -1,5 +1,4 @@
 import i18n from '@dhis2/d2-i18n'
-import { PropTypes } from '@dhis2/prop-types'
 import {
     Button,
     ButtonStrip,
@@ -11,6 +10,7 @@ import {
     TabBar,
     Tab,
 } from '@dhis2/ui'
+import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 import styles from './TabControls.module.css'
 
