266dc49423516189cf4dee706cc78e720af744e2
Thomas Zemp <tom@dhis2.org>
2024-06-24 11:13:03 +0200

    fix: update plugin sizing definition (#1383)

--- services/plugin/src/Plugin.tsx
+++ services/plugin/src/Plugin.tsx
@@ -153,22 +153,15 @@ export const Plugin = ({
 
     if (pluginEntryPoint) {
         return (
-            <div
+            <iframe
+                ref={iframeRef}
+                src={pluginSource}
                 style={{
-                    height: `${pluginHeight}px`,
                     width: `${pluginWidth}px`,
+                    height: `${pluginHeight}px`,
+                    border: 'none',
                 }}
-            >
-                <iframe
-                    ref={iframeRef}
-                    src={pluginSource}
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        border: 'none',
-                    }}
-                ></iframe>
-            </div>
+            ></iframe>
         )
     }
 
