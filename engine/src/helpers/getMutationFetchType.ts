3df50ed2a67417e190143721b96e011a64e66e70
Austin McGee <947888+amcgee@users.noreply.github.com>
2024-11-25 14:50:54 +0100

    style: format

--- engine/src/helpers/getMutationFetchType.ts
+++ engine/src/helpers/getMutationFetchType.ts
@@ -2,7 +2,7 @@ import { FetchType } from '../types/ExecuteOptions'
 import { Mutation } from '../types/Mutation'
 
 export const getMutationFetchType = (mutation: Mutation): FetchType => {
-    if ( mutation.type === 'update') {
+    if (mutation.type === 'update') {
         return mutation.partial ? 'update' : 'replace'
     }
     return mutation.type
