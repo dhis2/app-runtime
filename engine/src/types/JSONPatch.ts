a90d5f727b99a07b86848bf81348933d3909977d
Austin McGee <947888+amcgee@users.noreply.github.com>
2025-10-30 18:09:33 +0100

    style: format

--- engine/src/types/JSONPatch.ts
+++ engine/src/types/JSONPatch.ts
@@ -1,28 +1,32 @@
 export const JSON_PATCH_CONTENT_TYPE = 'application/json-patch+json'
 
 export type JSONPatchOpAdd = {
-    op: 'add',
-    path: string,
+    op: 'add'
+    path: string
     value: any
 }
 
 export type JSONPatchOpRemove = {
-    op: 'remove',
-    path: string,
+    op: 'remove'
+    path: string
 }
 
 export type JSONPatchOpReplace = {
-    op: 'replace',
-    path: string,
+    op: 'replace'
+    path: string
     value: any
 }
 
 export type JSONPatchOpRemoveById = {
-    op: 'remove-by-id',
-    path: string,
+    op: 'remove-by-id'
+    path: string
     id: string
 }
 
-export type JSONPatchOp = JSONPatchOpAdd | JSONPatchOpRemove | JSONPatchOpReplace | JSONPatchOpRemoveById
+export type JSONPatchOp =
+    | JSONPatchOpAdd
+    | JSONPatchOpRemove
+    | JSONPatchOpReplace
+    | JSONPatchOpRemoveById
 
 export type JSONPatch = JSONPatchOp[]
